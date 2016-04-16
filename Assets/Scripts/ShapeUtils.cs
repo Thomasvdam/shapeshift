11475e9ab389fdbe91d42b806ee966b44be6569e
Thomas van Dam <t.thomas.van.dam@gmail.com>
2016-04-16 17:13:36 +0200

    Add basic jump functionality.

---
+++ Assets/Scripts/ShapeUtils.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class ShapeUtils {
+	public static bool IsOnGround (GameObject player) {
+		RaycastHit2D hit = Physics2D.Raycast (player.transform.position, Vector2.down, GetDistanceToGround(player) + 0.1f);
+
+		if (hit.collider && hit.collider.gameObject.tag == "Platform") {
+			return true;
+		}
+
+		return false;
+	}
+
+	private static float GetDistanceToGround (GameObject player) {
+		return player.GetComponent<BoxCollider2D> ().bounds.extents.y;
+	}
+}
