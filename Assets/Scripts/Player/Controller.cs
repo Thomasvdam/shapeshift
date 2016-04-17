7184d7ff311faa26117f6fa7fc7de51ad65ecba4
Tamara Roep <trp300@gmail.com>
2016-04-17 23:33:22 +0200

    You can now jump from players. Grapplinghook can now pull players. Controller has some utility functions.

--- Assets/Scripts/Player/Controller.cs
+++ Assets/Scripts/Player/Controller.cs
@@ -22,19 +22,6 @@ public class Controller : MonoBehaviour {
 	public const string PS_PS4 = " button 12";
 	public const string TOUCH_PAD_PS4 = " button 13";
 
-	public const string SQUARE_PS3 = " button 3";
-	public const string CROSS_PS3 = " button 2";
-	public const string CIRCLE_PS3 = " button 1";
-	public const string TRIANGLE_PS3 = " button 0";
-	public const string LEFT_BUMPER_PS3 = " button 4";
-	public const string RIGHT_BUMPER_PS3 = " button 5";
-	public const string LEFT_TRIGGER_PS3 = " button 6";
-	public const string RIGHT_TRIGGER_PS3 = " button 7";
-	public const string START_PS3 = " button 9";
-	public const string L3_PS3 = " button 10";
-	public const string R3_PS3 = " button 11";
-	public const string PS_PS3 = " button 12";
-
 	private Shape shape;
 
 	void Start() {
@@ -66,66 +53,66 @@ public class Controller : MonoBehaviour {
 		shape.Move (x, y);
 	}
 
-	private bool checkButton(string ps4, string ps3) {
-		debugJoystickNames ();
-		if (string.Equals(Input.GetJoystickNames()[mId - 1], "PLAYSTATION(R)3 Controller")) {
-			Debug.Log ("ps3 controller found:" + mId);
-			return Input.GetKeyDown(getJoystickButton(ps3));
-		} else {
-			return Input.GetKeyDown(getJoystickButton(ps4));
-		}
+	public float getXAxisRaw() {
+		return Input.GetAxisRaw (CONTROLLER_PREFIX + mId + " X axis");
 	}
 
-	private void debugJoystickNames() {
-		for (int i = 0; i<Input.GetJoystickNames().Length; i++) {
-			Debug.Log ("name: " + Input.GetJoystickNames()[i] + ", i: " + i + ", checking id: " + mId);
-		}
+	public float getYAxisRaw() {
+		return Input.GetAxisRaw (CONTROLLER_PREFIX + mId + " Y axis");
 	}
-	
+
+	public bool getKeyDown(string ps4) {
+			return Input.GetKeyDown(getJoystickButton(ps4));
+	}
+
+	public bool getKey(string ps4) {
+		return Input.GetKey (getJoystickButton (ps4));
+	}
+
 	private void checkCrossButton() {
-		if (checkButton(CROSS_PS4, CROSS_PS3)) {
+		if (getKeyDown(CROSS_PS4)) {
 			shape.CrossPressed();
 		}
 	}
 
 	private void checkSquareButton() {
-		if (checkButton(SQUARE_PS4, SQUARE_PS3)) {
+		if (getKeyDown(SQUARE_PS4)) {
 			shape.SquarePressed();
 		}
 	}
 
 	private void checkCircleButton() {
-		if (checkButton(CIRCLE_PS4, CIRCLE_PS3)) {
+		if (getKeyDown(CIRCLE_PS4)) {
 			shape.CirclePressed();
 		}
 	}
 
 	private void checkTriangleButton() {
-		if (checkButton(TRIANGLE_PS4, TRIANGLE_PS3)) {
+		if (getKeyDown(TRIANGLE_PS4)) {
 			shape.TrianglePressed();
 		}
 	}
 
 	private void checkLeftBumperButton() {
-		if (checkButton(LEFT_BUMPER_PS4, LEFT_BUMPER_PS3)) {
+		if (getKeyDown(LEFT_BUMPER_PS4)) {
 			shape.LeftBumperPressed();
 		}
 	}
 
 	private void checkRightBumperButton() {
-		if (checkButton(RIGHT_BUMPER_PS4, RIGHT_BUMPER_PS3)) {
+		if (getKeyDown(RIGHT_BUMPER_PS4)) {
 			shape.RightBumperPressed();
 		}
 	}
 
 	private void checkLeftTriggerButton() {
-		if (checkButton(LEFT_TRIGGER_PS4, LEFT_TRIGGER_PS3)) {
+		if (getKeyDown(LEFT_TRIGGER_PS4)) {
 			shape.LeftTriggerPressed();
 		}
 	}
 
 	private void checkRightTriggerButton() {
-		if (checkButton(RIGHT_TRIGGER_PS4, RIGHT_TRIGGER_PS3)) {
+		if (getKeyDown(RIGHT_TRIGGER_PS4)) {
 			shape.RightTriggerPressed();
 		}
 	}
