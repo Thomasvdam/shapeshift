7dadc3bb789ea5bab69d0f5f999dfe5ba096f59d
Thomas van Dam <t.thomas.van.dam@gmail.com>
2016-04-17 17:30:46 +0200

    Add audiomanager and placeholder death sound for the player, as well as an AudioManager.

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+	public static AudioManager instance = null;
+
+	AudioSource audio = null;
+
+	void Awake () {
+		//Check if there is already an instance of SoundManager
+		if (instance == null) {
+			//if not, set it to this.
+			instance = this;
+			audio = GetComponent<AudioSource> ();
+		}
+		//If instance already exists:
+		else if (instance != this) {
+			//Destroy this, this enforces our singleton pattern so there can only be one instance of SoundManager.
+			Destroy (gameObject);
+		}
+
+		//Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
+		DontDestroyOnLoad (gameObject);
+	}
+
+	public void PlaySound (AudioClip sound) {
+		if (sound) {
+			audio.PlayOneShot (sound, 1f);
+		}
+	}
+}
