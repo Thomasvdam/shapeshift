4b11a1b05085240abd04f6516334134259604558
Tamara Roep <trp300@gmail.com>
2016-04-17 18:24:39 +0200

    Controller script modifications. Added support for ps3.

--- Assets/Scripts/Player/Controller.cs
+++ Assets/Scripts/Player/Controller.cs
@@ -5,21 +5,41 @@ public class Controller : MonoBehaviour {
 
 	//Controller 1 to 4
 	public int mId = 1; //Default id
-	private const string SQUARE = " button 0";
-	private const string CROSS = " button 1";
-	private const string CIRCLE = " button 2";
-	private const string TRIANGLE = " button 3";
-	private const string LEFT_BUMPER = " button 4";
-	private const string RIGHT_BUMPER = " button 5";
-	private const string LEFT_TRIGGER = " button 6";
-	private const string RIGHT_TRIGGER = " button 7";
-	private const string SHARE = " button 8";
-	private const string OPTIONS = " button 9";
-	private const string L3 = " button 10";
-	private const string R3 = " button 11";
-	private const string PS = " button 12";
-	private const string TOUCH_PAD = " button 13";
-	private const string CONTROLLER_PREFIX = "joystick ";
+	public const string CONTROLLER_PREFIX = "joystick ";
+	
+	public const string SQUARE_PS4 = " button 0";
+	public const string CROSS_PS4 = " button 1";
+	public const string CIRCLE_PS4 = " button 2";
+	public const string TRIANGLE_PS4 = " button 3";
+	public const string LEFT_BUMPER_PS4 = " button 4";
+	public const string RIGHT_BUMPER_PS4 = " button 5";
+	public const string LEFT_TRIGGER_PS4 = " button 6";
+	public const string RIGHT_TRIGGER_PS4 = " button 7";
+	public const string SHARE_PS4 = " button 8";
+	public const string OPTIONS_PS4 = " button 9";
+	public const string L3_PS4 = " button 10";
+	public const string R3_PS4 = " button 11";
+	public const string PS_PS4 = " button 12";
+	public const string TOUCH_PAD_PS4 = " button 13";
+
+	public const string SQUARE_PS3 = " button 3";
+	public const string CROSS_PS3 = " button 2";
+	public const string CIRCLE_PS3 = " button 1";
+	public const string TRIANGLE_PS3 = " button 0";
+	public const string LEFT_BUMPER_PS3 = " button 4";
+	public const string RIGHT_BUMPER_PS3 = " button 5";
+	public const string LEFT_TRIGGER_PS3 = " button 6";
+	public const string RIGHT_TRIGGER_PS3 = " button 7";
+	public const string START_PS3 = " button 9";
+	public const string L3_PS3 = " button 10";
+	public const string R3_PS3 = " button 11";
+	public const string PS_PS3 = " button 12";
+
+	private Shape shape;
+
+	void Start() {
+		shape = gameObject.GetComponent<Shape> ();
+	}
 
 	void Update () {
 		checkCrossButton ();
@@ -43,54 +63,70 @@ public class Controller : MonoBehaviour {
 		if (x == 0 && y == 0) {
 			return;
 		}
-		gameObject.GetComponent<Shape>().Move (x, y);
+		shape.Move (x, y);
+	}
+
+	private bool checkButton(string ps4, string ps3) {
+		debugJoystickNames ();
+		if (string.Equals(Input.GetJoystickNames()[mId - 1], "PLAYSTATION(R)3 Controller")) {
+			Debug.Log ("ps3 controller found:" + mId);
+			return Input.GetKeyDown(getJoystickButton(ps3));
+		} else {
+			return Input.GetKeyDown(getJoystickButton(ps4));
+		}
+	}
+
+	private void debugJoystickNames() {
+		for (int i = 0; i<Input.GetJoystickNames().Length; i++) {
+			Debug.Log ("name: " + Input.GetJoystickNames()[i] + ", i: " + i + ", checking id: " + mId);
+		}
 	}
 	
 	private void checkCrossButton() {
-		if (Input.GetKeyDown (getJoystickButton(CROSS))) {
-			gameObject.GetComponent<Shape>().CrossPressed();
+		if (checkButton(CROSS_PS4, CROSS_PS3)) {
+			shape.CrossPressed();
 		}
 	}
 
 	private void checkSquareButton() {
-		if (Input.GetKeyDown (getJoystickButton(SQUARE))) {
-			gameObject.GetComponent<Shape>().SquarePressed();
+		if (checkButton(SQUARE_PS4, SQUARE_PS3)) {
+			shape.SquarePressed();
 		}
 	}
 
 	private void checkCircleButton() {
-		if (Input.GetKeyDown (getJoystickButton(CIRCLE))) {
-			gameObject.GetComponent<Shape>().CirclePressed();
+		if (checkButton(CIRCLE_PS4, CIRCLE_PS3)) {
+			shape.CirclePressed();
 		}
 	}
 
 	private void checkTriangleButton() {
-		if (Input.GetKeyDown (getJoystickButton(TRIANGLE))) {
-			gameObject.GetComponent<Shape>().TrianglePressed();
+		if (checkButton(TRIANGLE_PS4, TRIANGLE_PS3)) {
+			shape.TrianglePressed();
 		}
 	}
 
 	private void checkLeftBumperButton() {
-		if (Input.GetKeyDown (getJoystickButton(LEFT_BUMPER))) {
-			gameObject.GetComponent<Shape>().LeftBumperPressed();
+		if (checkButton(LEFT_BUMPER_PS4, LEFT_BUMPER_PS3)) {
+			shape.LeftBumperPressed();
 		}
 	}
 
 	private void checkRightBumperButton() {
-		if (Input.GetKeyDown (getJoystickButton(RIGHT_BUMPER))) {
-			gameObject.GetComponent<Shape>().RightBumperPressed();
+		if (checkButton(RIGHT_BUMPER_PS4, RIGHT_BUMPER_PS3)) {
+			shape.RightBumperPressed();
 		}
 	}
 
 	private void checkLeftTriggerButton() {
-		if (Input.GetKeyDown (getJoystickButton(LEFT_TRIGGER))) {
-			gameObject.GetComponent<Shape>().LeftTriggerPressed();
+		if (checkButton(LEFT_TRIGGER_PS4, LEFT_TRIGGER_PS3)) {
+			shape.LeftTriggerPressed();
 		}
 	}
 
 	private void checkRightTriggerButton() {
-		if (Input.GetKeyDown (getJoystickButton(RIGHT_TRIGGER))) {
-			gameObject.GetComponent<Shape>().RightTriggerPressed();
+		if (checkButton(RIGHT_TRIGGER_PS4, RIGHT_TRIGGER_PS3)) {
+			shape.RightTriggerPressed();
 		}
 	}
 }
