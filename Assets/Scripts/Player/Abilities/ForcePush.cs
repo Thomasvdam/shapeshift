861004db34212c69a2e7b02c638adb53368b1e6a
Thomas van Dam <t.thomas.van.dam@gmail.com>
2016-04-17 23:27:29 +0200

    Add force push ability to the triangle shape.

---
+++ Assets/Scripts/Player/Abilities/ForcePush.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForcePush : MonoBehaviour {
+
+	public GameObject forceProjectile;
+	public float angle = 45f;
+	public int projectiles = 50;
+
+	public float jumpForce = 5f;
+
+	private float step;
+
+	void Start () {
+		this.step = this.angle / this.projectiles;
+	}
+
+	public void FireInDirection (Vector2 direction) {
+
+		// 90 correction to account for sprite 'forward' being right
+		float directionAngle = (Mathf.Atan2 (direction.x, direction.y) * Mathf.Rad2Deg) - 90f;
+		float currentAngle = directionAngle - (this.angle / 2f);
+
+		// Adjust angle slightly for all individual projectiles.
+		Quaternion rotation;
+		for (int x = 0; x < this.projectiles; x++) {
+			rotation = Quaternion.AngleAxis (currentAngle, Vector3.forward);
+
+			GameObject projectile = Instantiate (forceProjectile, this.transform.position, rotation) as GameObject;
+			projectile.GetComponent<ForcePushProjectile> ().Fire ();
+
+			Physics2D.IgnoreCollision (this.GetComponent<Collider2D> (), projectile.GetComponent<Collider2D> ());
+
+			// Increment for next iteration.
+			currentAngle += step;
+		}
+
+		// Give the player a slight boost when pushing downwards.
+		if (direction.y > 0.3f) {
+			Vector2 newDirection = new Vector2 (-1 * direction.x, direction.y);
+			this.GetComponent<Rigidbody2D> ().AddForce (newDirection * 100 * jumpForce);
+		}
+	}
+}
---
+++ Assets/Scripts/Player/ShapeScripts/TriangleShape.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriangleShape : Shape {
+
+	public float forceCooldown = 1.5f;
+	private float cooldownTimeStamp;
+
+	private ForcePush force;
+
+	override public void CustomStart () {
+		this.force = this.GetComponent<ForcePush> ();
+	}
+
+	override public void RightBumperPressed () {
+		if (Time.time <= this.cooldownTimeStamp) {
+			return;
+		}
+
+		// Normalize direction.
+		Vector2 normDirection = direction.normalized;
+
+		// Fire the force push.
+		force.FireInDirection (normDirection);
+
+		// Set cooldown.
+		this.cooldownTimeStamp = Time.time + this.forceCooldown;
+	}
+}
