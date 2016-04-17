4e041da6ce27b2c5df38c14e2659eba4b3e277d1
Maurits <maurits.ridder@gmail.com>
2016-04-18 01:07:34 +0200

    Add Lunge ability

---
+++ Assets/Scripts/Player/Abilities/Lunge.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lunge : MonoBehaviour {
+
+	public bool isLunging = false;
+	public Rigidbody2D squareRb;
+
+	private int lungeForce = 1000;
+
+	// Use this for initialization
+	void Start () {
+		squareRb = GetComponent<Rigidbody2D> ();
+	}
+
+	public void LungeSide(){
+
+		float x = Input.GetAxisRaw ("joystick 1 X axis");
+		x = x > 0 ? 1 : -1;
+		squareRb.AddForce (new Vector3 (lungeForce * x, 0.0f, 0.0f));
+		isLunging = true;
+
+	}
+
+	public IEnumerator LungeDuration (Collision2D col) {
+		
+		yield return new WaitForSeconds (0.4f);
+		col.collider.enabled = true;
+
+	}
+
+}
+
--- Assets/Scripts/Player/ShapeScripts/SquareShape.cs
+++ Assets/Scripts/Player/ShapeScripts/SquareShape.cs
@@ -3,4 +3,23 @@ using System.Collections;
 
 public class SquareShape : Shape {
 
+	private Lunge lunge;
+
+	override public void CustomStart (){
+		lunge = this.GetComponent<Lunge> ();
+	}
+
+	override public void SquarePressed(){
+		lunge.LungeSide ();
+	}
+
+	void OnCollisionEnter2D (Collision2D col) {
+
+		if (col.gameObject.tag == "Player" && lunge.isLunging) {
+			col.collider.enabled = false;
+			StartCoroutine (lunge.LungeDuration (col));
+			lunge.squareRb.velocity = Vector2.zero;
+
+		}
+	}
 }
