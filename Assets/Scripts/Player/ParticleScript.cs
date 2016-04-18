f345bbd6ce0ebf4bf5e691329ff9ec59f012c61e
Thomas van Dam <t.thomas.van.dam@gmail.com>
2016-04-18 22:16:25 +0200

    Fix particle system.

--- Assets/Scripts/Player/ParticleScript.cs
+++ Assets/Scripts/Player/ParticleScript.cs
@@ -7,13 +7,12 @@ public class ParticleScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		this.particles = this.gameObject.AddComponent<ParticleSystem> () as ParticleSystem;
+		this.particles = this.gameObject.GetComponent<ParticleSystem> () as ParticleSystem;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (particles.isStopped) {
-			Destroy (this.particles);
+		if (!particles.IsAlive ()) {
 			Destroy (this);
 		}
 	}
